aeebf975c4a85b8af178f0d2e127d9faba787843
Lucas Nahuel Espindola <lucas.nahuel.espindola1998@gmail.com>
2026-04-24 09:07:30 -0300

    update base url

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [react(), tailwindcss()],
-    base: '/portfolio/',
+    base: '/portfolio',
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
